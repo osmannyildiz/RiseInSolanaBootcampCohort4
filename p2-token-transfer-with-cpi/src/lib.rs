c5f3580bca2fe30b7b3f8e068cbe09d6d9d934d1
osmannyildiz <iamosmannyildiz@gmail.com>
2024-01-28 17:12:12 +0300

    initialize project 2 from template

---
+++ p2-token-transfer-with-cpi/src/lib.rs
@@ -0,0 +1,24 @@
+use {
+    solana_program::{
+        account_info::{next_account_info, AccountInfo},
+        entrypoint::ProgramResult,
+        msg,
+        program::invoke_signed,
+        program_error::ProgramError,
+        program_pack::Pack,
+        pubkey::Pubkey,
+    },
+    spl_token::{
+        instruction::transfer_checked,
+        state::{Account, Mint},
+    },
+};
+
+solana_program::entrypoint!(process_instruction);
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+   Ok(())
+}
