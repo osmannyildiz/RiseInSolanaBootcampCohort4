c6e65a463891ac96d64a9fbf079189965f25e4d4
osmannyildiz <iamosmannyildiz@gmail.com>
2024-01-28 15:46:37 +0300

    Counter II - Implementing Process Instruction

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,54 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use borsh_derive::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    // entrypoint::{self, ProgramResult}, // this doesn't work for some reason
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+use crate::instructions::CounterInstructions;
 
 pub mod instructions;
 
 #[derive(Debug, BorshDeserialize, BorshSerialize)]
 pub struct CounterAccount {
     pub counter: u32,
+}
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey, // contract identifier, like the contract address. since we didn't need it, we prefixed it with an underscore
+    accounts: &[AccountInfo], // accounts involved in this transaction. this must include CounterAccount
+    instructions_data: &[u8],
+) -> ProgramResult {
+    msg!("Counter program entry point");
+
+    let instruction: CounterInstructions = CounterInstructions::unpack(instructions_data)?;
+
+    let accounts_iter = &mut accounts.iter();
+    let account = next_account_info(accounts_iter)?;
+    // here we know that we have only one account to work with
+    // if we had more, we would need to loop and call next_account_info multiple times
+
+    let mut counter_account = CounterAccount::try_from_slice(&account.data.borrow())?;
+
+    match instruction {
+        CounterInstructions::Increment => {
+            counter_account.counter += 1;
+        }
+        CounterInstructions::Decrement => {
+            counter_account.counter -= 1;
+        }
+        CounterInstructions::Reset => {
+            counter_account.counter = 0;
+        }
+        CounterInstructions::Update(args) => counter_account.counter = args.value,
+    }
+
+    counter_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+    Ok(())
 }
